70cfa41fbbb3d43e7f56fe04df69df6ff3ca7045
pasechnik2307 <pasechnik2307@gmail.com>
2018-09-03 17:18:13 +0300

    task 1.9

---
+++ myapp.py
@@ -0,0 +1,10 @@
+def app(environ, start_response):
+    data = b"Hello, World!\n"
+    #data = "\n".join(environ.get('QUERY_STRING').split("&"))   
+
+
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
