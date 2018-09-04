2e5a25c8ef923b7f294e17d293a285b0185278d7
pasechnik2307 <pasechnik2307@gmail.com>
2018-09-04 10:01:49 +0300

    1.9.2

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
-from cgi import parse_qs
+#from cgi import parse_qs
 
-def app(environ, start_response):   
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    qs = parse_qs(environ['QUERY_STRING'])
-    return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
+#def app(environ, start_response):   
+ #   start_response("200 OK", [("Content-Type", "text/plain")])
+ #   qs = parse_qs(environ['QUERY_STRING'])
+ #   return ["\n".join(environ.get('QUERY_STRING').split("&"))]
+
+def hello_app(environ, start_response):
+    q_string = environ.get('QUERY_STRING')
+    pairs_list = q_string.split("&")
+    body = ""
+    for pair in pairs_list:
+        body += pair + "\n"
+
+    response_string = [('Content-type', 'text/plain')]
+
+    start_response('200 OK', response_string)
+    return [body.strip().encode()]
--- myapp.py
+++ myapp.py
@@ -1,10 +1,10 @@
 def app(environ, start_response):
-    data = b"Hello, World!\n"
-    #data = "\n".join(environ.get('QUERY_STRING').split("&"))   
-
+    #data = b"Hello, World!\n"
+    data = [b"\n".join(environ.get('QUERY_STRING').split("&"))]   
+    #data = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
-    return iter([data])
+    return data
