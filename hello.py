cc5020052a5ee0174a8966b7eb721422ee209675
pasechnik2307 <pasechnik2307@gmail.com>
2018-09-04 11:28:58 +0300

    1.9.4

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from cgi import parse_qs
-
-def app(environ, start_response):   
-    start_response("200 OK", [("Content-Type", "text/plain")])
-    #qs = parse_qs(environ['QUERY_STRING'])
-    return iter([b"\n".join(environ.get('QUERY_STRING').split("&"))])
-    #return['%s=%s\n' % (k, qs[k][0]) for k in qs]
+def app(environ, start_response):
+    env_data = environ.get("QUERY_STRING", "")
+    data = env_data.replace("&", "\n")
+    data = data.encode()
+    status = '200 OK'
+    response_headers = [('Content-type','text/plain')]
+    start_response(status, response_headers)
+    return [data]
